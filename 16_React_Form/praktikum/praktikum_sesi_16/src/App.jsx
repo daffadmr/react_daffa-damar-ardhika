a149910878b882a5335d83bf8c68f912a5caf7cc
daffadmr <daffaardhika@gmail.com>
2022-09-26 11:46:54 +0700

    task/16_React_Form: Add React form project

---
+++ 16_React_Form/praktikum/praktikum_sesi_16/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./Components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pendaftaran Peserta Coding Bootcamp</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
